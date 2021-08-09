a4a9e3f2fe6a74693990e3f3f4e4a90c42bd98aa
antonov-andrew <wtnm@rambler.ru>
2021-08-09 15:42:40 +0300

    move to class

--- __tests__/global-store.js
+++ __tests__/global-store.js
@@ -5,7 +5,7 @@ process.env.TS_NODE_PROJECT = './tsconfig.json';
 require('ts-mocha');
 const {expect} = require('chai');
 const sleep = require('sleep-promise');
-const createStore = require('../index.ts').default;
+const GlobalStore = require('../index.ts').default;
 require('jsdom-global')()
 const React = require("react");
 const {render} = require("react-dom");
@@ -13,7 +13,8 @@ const {act} = require("react-dom/test-utils");
 
 describe('test global store', function () {
   const initialState = {one: 1, two: 2};
-  let {subscribe, get, set, useSubscribe} = createStore(initialState);
+  let store = new GlobalStore(initialState);
+  let {subscribe, getValue, setValue, useSubscribe} = store;
 
   let container = null;
   container = document.createElement("div");
@@ -21,19 +22,19 @@ describe('test global store', function () {
 
 
   it('tests initial state', () => {
-    expect(get('one')).to.be.equal(initialState.one);
-    expect(get('two')).to.be.equal(initialState.two);
+    expect(getValue('one')).to.be.equal(initialState.one);
+    expect(getValue('two')).to.be.equal(initialState.two);
   })
 
   it('tests subscribe', async () => {
-    let unsub = subscribe(true, ([next, prev]) => {
+    let unsub = subscribe([], ([next, prev]) => {
       expect(prev.one).to.be.equal(1);
       expect(next.one).to.be.equal(11);
       expect(prev.three).to.be.equal(undefined);
       expect(next.three).to.be.equal(3);
     })
-    set('one', 11);
-    set('three', 3);
+    setValue('one', 11);
+    setValue('three', 3);
     await sleep(5);
     unsub();
   })
@@ -43,7 +44,7 @@ describe('test global store', function () {
 
     act(() => { render(React.createElement(TestGlobalState, {name: 'one'}), container); });
     expect(container.textContent).to.be.equal("11");
-    set('one', 10);
+    setValue('one', 10);
     await sleep(5);
     expect(container.textContent).to.be.equal("10");
   })
@@ -65,8 +66,8 @@ describe('test global store', function () {
     }
     act(() => { render(React.createElement(TestGlobalState, {req: {one: 'one', two: 'two'}}), container); });
     expect(container.textContent).to.be.equal('{"one":10,"two":2}');
-    set('one', 1);
-    set('two', 22);
+    setValue('one', 1);
+    setValue('two', 22);
     await sleep(5);
     expect(container.textContent).to.be.equal('{"one":1,"two":22}');
     act(() => { render(React.createElement(TestGlobalState, {req: {one: 'one', three: 'three'}}), container); });
@@ -80,8 +81,8 @@ describe('test global store', function () {
     }
     act(() => { render(React.createElement(TestGlobalState, {req: {four: ['four', 'five'], two: ['two']}}), container); });
     expect(container.textContent).to.be.equal('{"two":22}');
-    set('four', {five: 5});
-    set('six', {seven: 7});
+    setValue('four', {five: 5});
+    setValue('six', {seven: 7});
     await sleep(5);
     expect(container.textContent).to.be.equal('{"four":5,"two":22}');
     act(() => { render(React.createElement(TestGlobalState, {req: ['six', 'seven']}), container); });
@@ -90,25 +91,25 @@ describe('test global store', function () {
     expect(container.textContent).to.be.equal('{"seven":7}');
   })
 
-  let {set: setGS, useSubscribe: useGS} = createStore(initialState);
+  let store2 = new GlobalStore(initialState);
 
   it('tests useSubscribe with different stores', async () => {
-    const TestGlobalState = ({useHook, req}) => {
-      let val = useHook(req);
+    const TestGlobalState = ({store, req}) => {
+      let val = store.useSubscribe(req);
       return JSON.stringify(val);
     }
-    act(() => { render(React.createElement(TestGlobalState, {useHook: useSubscribe, req: 'two'}), container); });
+    act(() => { render(React.createElement(TestGlobalState, {store, req: 'two'}), container); });
     expect(container.textContent).to.be.equal('22');
 
-    act(() => { render(React.createElement(TestGlobalState, {useHook: useGS, req: 'two'}), container); });
+    act(() => { render(React.createElement(TestGlobalState, {store: store2, req: 'two'}), container); });
     expect(container.textContent).to.be.equal('2');
 
-    set('two', 222);
-    setGS('two', 21);
+    setValue('two', 222);
+    store2.setValue('two', 21);
     await sleep(5);
     expect(container.textContent).to.be.equal('21');
 
-    act(() => { render(React.createElement(TestGlobalState, {useHook: useSubscribe, req: 'two'}), container); });
+    act(() => { render(React.createElement(TestGlobalState, {store, req: 'two'}), container); });
     expect(container.textContent).to.be.equal('222');
   })
 })
--- index.ts
+++ index.ts
@@ -1,53 +1,94 @@
-import {useCallback, useEffect, useRef, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import {isEqual, isPromise, isFunction, isString, isObject, isArray, isMergeable} from 'is-fns';
 
-export default function (initialState: any = {}) {
-  const state: Map<string, any> = new Map(Object.entries(initialState));
-  const listeners = {};
-  const subscribers: Set<Function> = new Set();
-  let notify: null | number = null;
-  let notified = new Map();
-  let nextStateParts = {};
-  let prevStateParts = {};
+export default class GlobalStore {
+  private readonly state: Map<string, any>
+  private listeners = {}
+  private subscribers: Set<Function> = new Set()
+  private notify: null | number = null;
+  private notified = new Map();
+  private currentChanges = [{}, {}]; // [nextStateParts, prevStateParts]
+  private previousChanges = [{}, {}];
 
-  const notifyListeners = () => {
-    notify = null;
-    Object.keys(nextStateParts).forEach(key => {
-      if (listeners[key])
-        listeners[key].forEach((listener) => {
+  constructor(initialState: { [key: string]: any } = {}) {this.state = new Map(Object.entries(initialState))}
+
+  private notifyListeners = () => {
+    this.notify = null;
+    this.previousChanges = this.currentChanges;
+    this.currentChanges = [{}, {}];
+    Object.keys(this.previousChanges[0]).forEach(key => {
+      if (this.listeners[key])
+        this.listeners[key].forEach((listener) => {
           const {req, subscriber, previous} = listener;
-          if (!notified.get(subscriber)) {
-            let result = makeStateParts(req);
+          if (!this.notified.get(subscriber)) {
+            let result = this.makeStateParts(req);
             if (!isEqual(previous, result)) subscriber(listener.previous = result);
-            notified.set(subscriber, true);
+            this.notified.set(subscriber, true);
           }
         });
     })
-    subscribers.forEach(subscriber => subscriber([nextStateParts, prevStateParts]))
-    nextStateParts = {};
-    prevStateParts = {};
-    notified = new Map();
+    this.subscribers.forEach(subscriber => subscriber(this.previousChanges))
+    this.notified = new Map();
   }
 
-  const setStateAndNotifier = (key, value) => {
-    if (Object.is(state.get(key), value)) return;
-    if (!prevStateParts.hasOwnProperty(key)) prevStateParts[key] = state.get(key);
-    nextStateParts[key] = value;
-    state.set(key, value);
-    if (!notify) notify = setTimeout(notifyListeners, 0); // execute on next tick
+  private setStateAndNotifier = (key, value) => {
+    if (Object.is(this.state.get(key), value)) return;
+    if (!this.currentChanges[1].hasOwnProperty(key)) this.currentChanges[1][key] = this.state.get(key);
+    this.currentChanges[0][key] = value;
+    this.state.set(key, value);
+    if (!this.notify) this.notify = setTimeout(this.notifyListeners, 0); // execute on the next tick
   }
 
-  const setValue = (key, value) => {
-    if (isFunction(value)) value = value(state.get(key));
-    if (isPromise(value)) value.then((val) => setStateAndNotifier(key, val)).catch((e) => {throw new Error(e)})
-    else setStateAndNotifier(key, value)
+  private getStoreKey = (v) => isArray(v) ? v[0] : v;
+
+  private makeStateParts = (req) => (isString(req) || isArray(req)) ? this.getValue(req) : (!req ? req :
+    Object.keys(req).reduce((r, prop) => (r[prop] = this.getValue(req[prop])) && r || r, {}));
+
+  private usePrevReqAndState = (req, partState) => {
+    const {current} = useRef({});
+    if (!isEqual(req, current.req, {deep: true}) || current.state !== this.state) { // new subscription were made
+      current.state = this.state;
+      current.req = req;
+      current.check = partState;   // save old partState to check
+      current.result = this.makeStateParts(req);
+      return [current.req, current.result]
+    } else if (current.hasOwnProperty('check') && current.check === partState) // partState not yet updated
+      return [current.req, current.result]
+    else delete current.check;  // subscription made and partState updated
+    return [req, partState]
   }
 
-  const getInState = (keys) => {
-    if (isString(keys)) return state.get(keys);
+  useSubscribe = (req: false | string | string[] | { [key: string]: string | string[] }) => {
+    let [partState, setPartState] = useState(this.makeStateParts(req));
+    [req, partState] = this.usePrevReqAndState(req, partState);
+    useEffect(() => this.subscribe(req, setPartState), [req, this.state]);
+    return partState
+  };
+
+  subscribe = (req, subscriber) => {
+    if (isArray(req) && !req.length) return this.subscribers.add(subscriber) && (() => this.subscribers.delete(subscriber))
+    if (req === true || !req && req !== "") return req;
+    const subsObject = {req, subscriber};
+    const props = isObject(req) ? Object.keys(req) : [""];
+    props.forEach(prop => {
+      const key = this.getStoreKey(isObject(req) ? req[prop] : req);
+      if (!this.listeners[key]) this.listeners[key] = new Set();
+      this.listeners[key].add(subsObject);
+    });
+    return () => props.forEach(p => this.listeners[this.getStoreKey(isObject(req) ? req[p] : req)].delete(subsObject));
+  };
+
+  setValue = (key, value) => {
+    if (isFunction(value)) value = value(this.state.get(key));
+    if (isPromise(value)) value.then((val) => this.setStateAndNotifier(key, val)).catch((e) => {throw new Error(e)})
+    else this.setStateAndNotifier(key, value)
+  }
+
+  getValue = (keys) => {
+    if (isString(keys)) return this.state.get(keys);
     if (isArray(keys)) {
-      if (keys.length === 0) throw new Error('Request key length must be at least 1');
-      let res: any = state.get(keys[0]);
+      if (keys.length === 0) return this.previousChanges;
+      let res: any = this.state.get(keys[0]);
       for (let i = 1; i < keys.length; i++) {
         if (isMergeable(res)) res = res[keys[i]];
         else return undefined;
@@ -56,44 +97,5 @@ export default function (initialState: any = {}) {
     }
   }
 
-  const makeStateParts = (req) => (isString(req) || isArray(req)) ? getInState(req) : (!req ? req :
-    Object.keys(req).reduce((r, prop) => (r[prop] = getInState(req[prop])) && r || r, {}));
-
-  const usePrevReqAndState = (req, partState) => {
-    const {current} = useRef({});
-    if (!isEqual(req, current.req, {deep: true}) || current.state !== state) { // new subscription were made
-      current.state = state;
-      current.req = req;
-      current.check = partState;   // save old partState to check
-      current.result = makeStateParts(req);
-      return [current.req, current.result]
-    } else if (current.hasOwnProperty('check') && current.check === partState) // partState not yet updated
-      return [current.req, current.result]
-    else delete current.check;  // subscription made and partState updated
-    return [req, partState]
-  }
-
-  const useSubscribe = (req: string | string[] | { [key: string]: string | string[] }) => {
-    let [partState, setPartState] = useState(makeStateParts(req));
-    [req, partState] = usePrevReqAndState(req, partState);
-    useEffect(() => subscribe(req, setPartState), [req, state]);
-    return partState
-  };
-
-  const getStoreKey = (v) => isArray(v) ? v[0] : v;
-
-  const subscribe = (req, subscriber) => {
-    if (req === true) return subscribers.add(subscriber) && (() => subscribers.delete(subscriber))
-    if (!req && req !== "") return req;
-    const subsObject = {req, subscriber};
-    const props = isObject(req) ? Object.keys(req) : [""];
-    props.forEach(prop => {
-      const key = getStoreKey(isObject(req) ? req[prop] : req);
-      if (!listeners[key]) listeners[key] = new Set();
-      listeners[key].add(subsObject);
-    });
-    return () => props.forEach(p => listeners[getStoreKey(isObject(req) ? req[p] : req)].delete(subsObject));
-  };
-
-  return {useSubscribe, subscribe, set: setValue, get: getInState, keys: () => state.keys()};
+  keys = () => this.state.keys()
 }
