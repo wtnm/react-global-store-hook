9167fd5567f348058fbe0b2368c887a1d3b34bcf
antonov-andrew <wtnm@rambler.ru>
2021-08-16 15:49:32 +0300

    bugs fix

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import {useEffect, useRef, useState} from 'react';
-import {isEqual, isPromise, isFunction, isString, isObject, isArray, isMergeable} from 'is-fns';
+import {isEqual, isPromise, isFunction, isString, isObject, isArray, isMergeable, isUndefined} from 'is-fns';
 
 export default class GlobalStore {
   private readonly state: Map<string, any>
@@ -35,14 +35,27 @@ export default class GlobalStore {
     if (Object.is(this.state.get(key), value)) return;
     if (!this.currentChanges[1].hasOwnProperty(key)) this.currentChanges[1][key] = this.state.get(key);
     this.currentChanges[0][key] = value;
-    this.state.set(key, value);
+    if (isUndefined(value)) {this.state.delete(key)} else {this.state.set(key, value)}
     if (!this.notify) this.notify = setTimeout(this.notifyListeners, 0); // execute on the next tick
   }
 
   private getStoreKey = (v) => isArray(v) ? v[0] : v;
 
-  private makeStateParts = (req) => (isString(req) || isArray(req)) ? this.get(req) : (!req ? req :
-    Object.keys(req).reduce((r, prop) => (r[prop] = this.get(req[prop])) && r || r, {}));
+  private getValue = (keys) => {
+    if (isString(keys)) return this.state.get(keys);
+    if (isArray(keys)) {
+      if (keys.length === 0) return this.previousChanges;
+      let res: any = this.state.get(keys[0]);
+      for (let i = 1; i < keys.length; i++) {
+        if (isMergeable(res)) res = res[keys[i]];
+        else return undefined;
+      }
+      return res;
+    }
+  }
+
+  private makeStateParts = (req) => (isString(req) || isArray(req)) ? this.getValue(req) : (!req ? req :
+    Object.keys(req).reduce((r, prop) => (r[prop] = this.getValue(req[prop])) && r || r, {}));
 
   private usePrevReqAndState = (req, partState) => {
     const {current} = useRef({});
@@ -52,9 +65,10 @@ export default class GlobalStore {
       current.check = partState;   // save old partState to check
       current.result = this.makeStateParts(req);
       return [current.req, current.result]
-    } else if (current.hasOwnProperty('check') && current.check === partState) // partState not yet updated
-      return [current.req, current.result]
-    else delete current.check;  // subscription made and partState updated
+    } else if (current.hasOwnProperty('check') && current.check === partState) { // partState not yet updated
+      if (isObject(req) || this.makeStateParts(req) !== partState) return [current.req, current.result];
+    }
+    delete current.check;  // subscription made and partState updated
     return [req, partState]
   }
 
@@ -84,18 +98,7 @@ export default class GlobalStore {
     else this.setStateAndNotifier(key, value)
   }
 
-  get = (keys) => {
-    if (isString(keys)) return this.state.get(keys);
-    if (isArray(keys)) {
-      if (keys.length === 0) return this.previousChanges;
-      let res: any = this.state.get(keys[0]);
-      for (let i = 1; i < keys.length; i++) {
-        if (isMergeable(res)) res = res[keys[i]];
-        else return undefined;
-      }
-      return res;
-    }
-  }
+  get = (req) => this.makeStateParts(req)
 
   keys = () => this.state.keys()
 }
